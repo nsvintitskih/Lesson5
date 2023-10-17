b8c89eb6db10d2504cec8d66e6809b233d31583d
nsvintitskih <nsvintitskih@yandex.ru>
2023-10-17 23:45:40 +0300

    Lesson5

--- src/dz/Main.java
+++ src/dz/Main.java
@@ -1,2 +1,40 @@
-package dz;public class Main {
+package dz;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int nam = scanner.nextInt();
+        int nam2 = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        switch (d){
+            case 1:
+                System.out.printf("+ %d", nam+nam2 );
+                break;
+            case 2:
+                System.out.printf("- %d", nam-nam2);
+                break;
+            case 3:
+                System.out.printf("* %d", nam*nam2);
+                break;
+            case 4:
+                System.out.printf("/ %.2f", (double)nam/nam2);
+                break;
+            default:
+                System.out.println("Error");
+        }
+        System.out.println();
+
+        if (d == 1){
+            System.out.printf("+ %d", nam+nam2);
+        }else if (d == 2) {
+            System.out.printf("- %d", nam + nam2);
+        }else{
+            System.out.println("Error");
+        }  scanner.close();
+    }
 }
